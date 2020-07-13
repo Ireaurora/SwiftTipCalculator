2b854ae5a7cd3dffc880a058e2d8a4258c1a620e
Ireaurora <ireaurora@gmail.com>
2020-07-13 18:46:01 +0100

    Added alert form

--- TipCalculator/ViewController.swift
+++ TipCalculator/ViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -29,7 +29,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var tipOutlet: UILabel!
     @IBOutlet weak var totalCostOutlet: UILabel!
     @IBOutlet weak var costPerPersonOutlet: UILabel!
-
+    
     
     @IBAction func percentSlider(_ sender: UISlider) {
         sender.maximumValue = 100
@@ -44,41 +44,40 @@ class ViewController: UIViewController {
         percentageLabel.text = sender.titleLabel!.text
         percentageSlider.value = Float(String(Substring(sender.titleLabel!.text!).dropLast())) ?? 0.0
     }
-   
+    
     
     @IBAction func calculate(_ sender: UIButton) {
         if(amountTextField.text != "" && numberPeople.text != ""){
             let percentLabel = Float(String(Substring(percentageLabel.text ?? "0").dropLast()))
-                let  actualBill = amountTextField.text!
+            let  actualBill = amountTextField.text!
             calculateTip(percentLabel: percentLabel ?? 0.0, actualBill: actualBill ?? "0")
         }else{
-         var dialogMessage = UIAlertController(title: "Mandatory Fields", message: "Please complete all the fields", preferredStyle: .alert)
-         
-         let ok = UIAlertAction(title: "OK", style: .default, handler: { (action) -> Void in
-          })
-         
-         dialogMessage.addAction(ok)
-
-         self.present(dialogMessage, animated: true, completion: nil)
+            var dialogMessage = UIAlertController(title: "Mandatory Fields", message: "Please complete all the fields", preferredStyle: .alert)
+            
+            let ok = UIAlertAction(title: "OK", style: .default, handler: { (action) -> Void in
+            })
+            
+            dialogMessage.addAction(ok)
+            
+            self.present(dialogMessage, animated: true, completion: nil)
         }
     }
     
     func calculateTip(percentLabel: Float, actualBill:String){
-         let tip = Float(Double(actualBill)!) * Float(percentLabel)/100
+        let tip = Float(Double(actualBill)!) * Float(percentLabel)/100
         tipOutlet.text = String(tip)
         calculateTotalCost(tip: tip, actualBill: actualBill)
-      }
+    }
     
     
     func calculateTotalCost(tip:Float, actualBill:String){
-        totalCostOutlet.text = String(Float(Double(tip)) + Float(actualBill)!)
         let total = Float(Double(tip)) + Float(actualBill)!
+        totalCostOutlet.text = String(total)
         calculateCostPerson(total:actualBill)
     }
     
     func calculateCostPerson(total : String){
         let number = numberPeople.text!
-        print(total, number)
         costPerPersonOutlet.text = String(Float(Double(total)!) / Float(number)!)
     }
     
